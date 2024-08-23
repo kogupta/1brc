1299ef87b8888731d414446a8de1f08d932dd6c8
kogupta <kohinoor.gupta@gmail.com>
2024-08-23 16:51:40 +0530

    1brc: mmap, chunked - ~40 sec; fastest no unsafe (gonix) runs about 8~ seconds in local.

--- calculate_average_kogupta.sh
+++ calculate_average_kogupta.sh
@@ -16,26 +16,36 @@
 #
 
 source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk use java 21.0.3-graal 1>&2
 
-USE_NATIVE_IMAGE=${2:-false}
+USE_GRAAL=${1:-false}
+
+if [ "$USE_GRAAL" = true ]; then
+    sdk use java 21.0.3-graal 1>&2
+else
+    sdk use java 21.0.4-oracle 1>&2
+fi
 
 EXEC=CalculateAverage_kogupta
-IMAGE="$EXEC"_image
 
-if [ ! -f "target/$IMAGE" ] && [ "$USE_NATIVE_IMAGE" = true ]; then
-    NATIVE_IMAGE_OPTS="--gc=epsilon -O3 -H:+UnlockExperimentalVMOptions -H:-GenLoopSafepoints -march=native --enable-preview -H:InlineAllBonus=10 -H:-ParseRuntimeOptions --initialize-at-build-time=dev.morling.onebrc.$EXEC"
+#USE_NATIVE_IMAGE=${1:-false}
+#IMAGE="$EXEC"_image
+#
+#if [ ! -f "target/$IMAGE" ] && [ "$USE_NATIVE_IMAGE" = true ]; then
+#    NATIVE_IMAGE_OPTS="--gc=epsilon -O3 -H:+UnlockExperimentalVMOptions -H:-GenLoopSafepoints -march=native --enable-preview -H:InlineAllBonus=10 -H:-ParseRuntimeOptions --initialize-at-build-time=dev.morling.onebrc.$EXEC"
+#
+#    native-image "$NATIVE_IMAGE_OPTS" \
+#      -cp target/average-1.0.0-SNAPSHOT.jar \
+#      -o target/$IMAGE dev.morling.onebrc.$EXEC
+#fi
 
-    native-image "$NATIVE_IMAGE_OPTS" \
-      -cp target/average-1.0.0-SNAPSHOT.jar \
-      -o target/$IMAGE dev.morling.onebrc.$EXEC
-fi
+#if [ -f target/"$IMAGE" ]; then
+#    echo "Using native image $IMAGE" 1>&2
+#    hyperfine -w 0 -r 5 target/"$IMAGE" "$FILE"
+#else
+#    echo "Native image not found, using JVM mode." 1>&2
+#    JAVA_OPTS="--enable-preview -Xlog:gc*:gc.log -XX:StartFlightRecording=dumponexit=true,filename=recording%t.jfr"
+#    /usr/bin/time -p java $JAVA_OPTS -cp target/average-1.0.0-SNAPSHOT.jar dev.morling.onebrc.$EXEC
+#fi
 
-if [ -f target/"$IMAGE" ]; then
-    echo "Using native image $IMAGE" 1>&2
-    hyperfine -w 0 -r 5 target/"$IMAGE" "$FILE"
-else
-    echo "Native image not found, using JVM mode." 1>&2
-    JAVA_OPTS="--enable-preview"
-    java $JAVA_OPTS -cp target/average-1.0.0-SNAPSHOT.jar dev.morling.onebrc.$EXEC
-fi
+
+hyperfine -w 2 -r 5 "java -Xms1G -Xmx1G -cp target/average-1.0.0-SNAPSHOT.jar dev.morling.onebrc.$EXEC"
